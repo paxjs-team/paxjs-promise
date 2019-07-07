ba7f910d4815741d01c283e74517b4202dd7325c
serhiipalash <serhii.palash@gmail.com>
2019-07-07 17:16:55 +0300

    Do nothing if Promise object is not available

--- src/delay.js
+++ src/delay.js
@@ -1,31 +1,33 @@
-Promise.delay = function(ms, value) {
-  if (ms === undefined || ms < 0) {
-    return Promise.resolve(value)
-  }
-
-  if (value instanceof Promise) {
-    return value.then(function(result) {
-      return new Promise(function(resolve) {
-        setTimeout(resolve, ms, result)
-      })
-    })
-  }
-
-  return new Promise(function(resolve) {
-    setTimeout(resolve, ms, value)
-  })
-}
-
-Promise.prototype.delay = function(ms) {
-  return this.then(function(data) {
+if (Promise) {
+  Promise.delay = function(ms, value) {
     if (ms === undefined || ms < 0) {
-      return data
+      return Promise.resolve(value)
+    }
+
+    if (value instanceof Promise) {
+      return value.then(function(result) {
+        return new Promise(function(resolve) {
+          setTimeout(resolve, ms, result)
+        })
+      })
     }
 
     return new Promise(function(resolve) {
-      setTimeout(function() {
-        resolve(data)
-      }, ms)
+      setTimeout(resolve, ms, value)
     })
-  })
+  }
+
+  Promise.prototype.delay = function(ms) {
+    return this.then(function(data) {
+      if (ms === undefined || ms < 0) {
+        return data
+      }
+
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          resolve(data)
+        }, ms)
+      })
+    })
+  }
 }
--- src/time.js
+++ src/time.js
@@ -1,68 +1,72 @@
-Promise.TimeError = function TimeError(msg) {
-  this.message = msg
-  this.name = 'TimeError'
+if (Promise) {
+  Promise.TimeError = function TimeError(msg) {
+    this.message = msg
+    this.name = 'TimeError'
 
-  if (Error.captureStackTrace) {
-    Error.captureStackTrace(this, this.constructor)
-  } else {
-    Error.call(this)
-  }
-}
-
-Promise.TimeError.prototype = Object.create(Error.prototype)
-
-Promise.prototype.time = function(options) {
-  var minTimePromise
-
-  if (
-    options === null ||
-    typeof options !== 'object' ||
-    (!options.min && !options.max)
-  ) {
-    return this
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor)
+    } else {
+      Error.call(this)
+    }
   }
 
-  if (options.min > options.max) {
-    return Promise.reject(
-      new Promise.TimeError("operation max time can't be less than min time")
-    )
-  }
+  Promise.TimeError.prototype = Object.create(Error.prototype)
 
-  if (options.min >= 0) {
-    minTimePromise = Promise.all([
-      this,
+  Promise.prototype.time = function(options) {
+    var minTimePromise
 
-      new Promise(function(resolve) {
-        setTimeout(resolve, options.min)
-      }),
-    ]).then(function(result) {
-      return result[0]
-    })
-  } else {
-    minTimePromise = this
-  }
+    if (
+      options === null ||
+      typeof options !== 'object' ||
+      (!options.min && !options.max)
+    ) {
+      return this
+    }
 
-  if (options.max >= 0) {
-    return new Promise(function(resolve, reject) {
-      var maxTimeoutId = setTimeout(function() {
-        reject(
-          new Promise.TimeError(options.timeoutMessage || 'operation timed out')
-        )
-      }, options.max)
+    if (options.min > options.max) {
+      return Promise.reject(
+        new Promise.TimeError("operation max time can't be less than min time")
+      )
+    }
 
-      minTimePromise
-        .then(function(result) {
-          clearTimeout(maxTimeoutId)
+    if (options.min >= 0) {
+      minTimePromise = Promise.all([
+        this,
 
-          resolve(result)
-        })
-        .catch(function(err) {
-          clearTimeout(maxTimeoutId)
+        new Promise(function(resolve) {
+          setTimeout(resolve, options.min)
+        }),
+      ]).then(function(result) {
+        return result[0]
+      })
+    } else {
+      minTimePromise = this
+    }
 
-          reject(err)
-        })
-    })
-  } else {
-    return minTimePromise
+    if (options.max >= 0) {
+      return new Promise(function(resolve, reject) {
+        var maxTimeoutId = setTimeout(function() {
+          reject(
+            new Promise.TimeError(
+              options.timeoutMessage || 'operation timed out'
+            )
+          )
+        }, options.max)
+
+        minTimePromise
+          .then(function(result) {
+            clearTimeout(maxTimeoutId)
+
+            resolve(result)
+          })
+          .catch(function(err) {
+            clearTimeout(maxTimeoutId)
+
+            reject(err)
+          })
+      })
+    } else {
+      return minTimePromise
+    }
   }
 }
